13b4501face56260cc50a8eb897c9cd92f59d314
SweetXiaoXuan <2459374088@qq.com>
2018-02-23 12:14:47 +0800

    删除test内容

--- src/test/com/emoji/EmojiTest.java
+++
@@ -1,8 +0,0 @@
-package com.emoji;
-
-public class EmojiTest {
-    public static void main(String[] args) {
-//        String emojiString = emojiParser.emojiText(emojitextview.getText())
-//        System.out.println(emojiString);
-    }
-}
--- src/test/java/TestErrorBean.java
+++
@@ -1,4 +0,0 @@
-
-public class TestErrorBean {
-    public String error;
-}
